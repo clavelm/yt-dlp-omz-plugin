66993ee6594beee817105db4c92d0f5d90ed572a
Mathieu CLAVEL <math.clavel@protonmail.com>
2023-03-21 13:16:50 +0100

    feat: add alias mkv and set parallel download to 10 as default

--- yt-dlp.plugin.zsh
+++ yt-dlp.plugin.zsh
@@ -4,6 +4,7 @@ function y() {
   local outputFormat="%(upload_date)s - %(uploader)s - %(title)s [%(width)sx%(height)s %(fps)s][%(id)s].%(ext)s"
   local arcAlias="--download-archive done.txt"
   local subAlias="--sub-lang 'en.*' --convert-subs srt --merge-output-format mkv --embed-subs"
-  yt-dlp -o $outputFormat --alias arc $arcAlias --alias sub $subAlias "$@"
+  local mkvAlias="--remux-video mkv"
+  yt-dlp -o $outputFormat --alias arc $arcAlias --alias sub $subAlias --alias mkv $mkvAlias -N 10 "$@"
 }
 compdef y=yt-dlp
