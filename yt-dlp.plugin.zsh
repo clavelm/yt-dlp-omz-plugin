7abc9ee76aeb3f653f86569f7646b07bed42dbbb
Mathieu CLAVEL <math.clavel@protonmail.com>
2023-04-03 11:10:51 +0200

    feat(alias): arc by default, narc for no archive, sub use mkv

--- yt-dlp.plugin.zsh
+++ yt-dlp.plugin.zsh
@@ -2,9 +2,9 @@ unalias y 2> /dev/null || true
 
 function y() {
   local outputFormat="%(upload_date)s - %(uploader)s - %(title)s [%(width)sx%(height)s %(fps)s][%(id)s].%(ext)s"
-  local arcAlias="--download-archive done.txt"
-  local subAlias="--sub-lang 'en.*' --convert-subs srt --merge-output-format mkv --embed-subs"
+  local noArcAlias="--no-download-archive"
+  local subAlias="--sub-lang 'en.*' --convert-subs srt --mkv --embed-subs"
   local mkvAlias="--remux-video mkv"
-  yt-dlp -o $outputFormat --alias arc $arcAlias --alias sub $subAlias --alias mkv $mkvAlias -N 10 "$@"
+  yt-dlp -o $outputFormat --alias narc $noArcAlias --alias sub $subAlias --alias mkv $mkvAlias -N 10 --download-archive "done.txt"  "$@"
 }
 compdef y=yt-dlp
