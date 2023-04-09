a67ccca9dbe8b703fe87834e40097c412fa3c3df
Mathieu CLAVEL <math.clavel@protonmail.com>
2023-04-09 21:51:03 +0200

    feat: add en-orig.* sub and auto-sub to --sub

--- yt-dlp.plugin.zsh
+++ yt-dlp.plugin.zsh
@@ -3,7 +3,7 @@ unalias y 2> /dev/null || true
 function y() {
   local outputFormat="%(upload_date)s - %(uploader)s - %(title)s [%(width)sx%(height)s %(fps)s][%(id)s].%(ext)s"
   local noArcAlias="--no-download-archive"
-  local subAlias="--sub-lang 'en.*' --convert-subs srt --mkv --embed-subs"
+  local subAlias="--sub-lang 'en.*,en-orig.*' --convert-subs srt --mkv --embed-subs --write-auto-subs"
   local mkvAlias="--remux-video mkv"
   yt-dlp -o $outputFormat --alias narc $noArcAlias --alias sub $subAlias --alias mkv $mkvAlias -N 10 --download-archive "done.txt"  "$@"
 }
