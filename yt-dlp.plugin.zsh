98d5148b062c65bdcc5a406aced689ce1672d17f
Mathieu CLAVEL <math.clavel@protonmail.com>
2024-02-18 17:00:37 +0100

    feat: embed chapters

--- yt-dlp.plugin.zsh
+++ yt-dlp.plugin.zsh
@@ -5,6 +5,6 @@ function y() {
   local noArcAlias="--no-download-archive"
   local subAlias="--sub-lang 'en.*,en-orig.*' --convert-subs srt --mkv --embed-subs --write-auto-subs"
   local mkvAlias="--remux-video mkv"
-  yt-dlp -o $outputFormat --alias narc $noArcAlias --alias sub $subAlias --alias mkv $mkvAlias -N 10 --download-archive "done.txt"  "$@"
+  yt-dlp -o $outputFormat --alias narc $noArcAlias --alias sub $subAlias --alias mkv $mkvAlias -N 10 --download-archive "done.txt" --embed-chapters  "$@"
 }
 compdef y=yt-dlp
