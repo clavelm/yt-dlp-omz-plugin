25606ca81b95e3acffb7861b3212a22aa6fdccc3
Mathieu CLAVEL <math.clavel@protonmail.com>
2024-03-22 17:07:48 +0100

    feat: add the loading of conf from ./yt.config.txt if present

--- yt-dlp.plugin.zsh
+++ yt-dlp.plugin.zsh
@@ -5,6 +5,13 @@ function y() {
   local noArcAlias="--no-download-archive"
   local subAlias="--sub-lang 'en.*,en-orig.*' --convert-subs srt --mkv --embed-subs --write-auto-subs"
   local mkvAlias="--remux-video mkv"
-  yt-dlp -o $outputFormat --alias narc $noArcAlias --alias sub $subAlias --alias mkv $mkvAlias -N 10 --download-archive "done.txt" --embed-chapters  "$@"
+  
+  local defParam="-N 10 --download-archive done.txt --embed-chapters"
+  
+  if [[ -f "./yt.config.txt" ]]; then
+    yt-dlp -o $outputFormat --alias narc $noArcAlias --alias sub $subAlias --alias mkv $mkvAlias -N 10 --download-archive "done.txt" --embed-chapters --config-locations "./yt.config.txt"  "$@"
+  else
+    yt-dlp -o $outputFormat --alias narc $noArcAlias --alias sub $subAlias --alias mkv $mkvAlias -N 10 --download-archive "done.txt" --embed-chapters  "$@"
+  fi
 }
 compdef y=yt-dlp
